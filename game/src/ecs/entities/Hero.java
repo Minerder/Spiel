251ce6f6ea83292d6cd74cca9e59432fb8b67b8a
Scepzz <120119982+Scepzz@users.noreply.github.com>
2023-04-17 14:57:25 +0200

    Update Hero.java

--- game/src/ecs/entities/Hero.java
+++ game/src/ecs/entities/Hero.java
@@ -34,7 +34,7 @@ public class Hero extends Entity {
         PlayableComponent pc = new PlayableComponent(this);
         setupFireballSkill();
         pc.setSkillSlot1(firstSkill);
-        // test
+        // test moooin
     }
 
     private void setupVelocityComponent() {
