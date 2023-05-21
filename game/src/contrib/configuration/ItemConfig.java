9e0010e6b1db1da1042db70e0619bafd18f00097
Edwin <dik.edwin122@gmail.com>
2023-05-21 21:08:47 +0200

    changed hotkeys
    - renamed first skill to equipment slot
    - renamed second and third skill to skill slot 1 and skill slot 2
    - removed fourth skill

--- game/src/contrib/configuration/ItemConfig.java
+++ game/src/contrib/configuration/ItemConfig.java
@@ -1,30 +1,39 @@
 package contrib.configuration;
 
-import contrib.utils.components.item.ItemType;
-
+import contrib.utils.components.item.ItemClassification;
 import core.configuration.ConfigKey;
 import core.configuration.ConfigMap;
 import core.configuration.values.ConfigEnumValue;
 import core.configuration.values.ConfigStringValue;
 
-/** The default ItemData values */
+/**
+ * The default ItemData values
+ */
 @ConfigMap(path = {"item"})
 public class ItemConfig {
-    /** The Description of the Default ItemData */
+    /**
+     * The Description of the Default ItemData
+     */
     public static final ConfigKey<String> DESCRIPTION =
-            new ConfigKey<>(new String[] {"description"}, new ConfigStringValue("Default Item"));
+        new ConfigKey<>(new String[]{"description"}, new ConfigStringValue("Default Item"));
 
-    /** The Name of the Default ItemData */
+    /**
+     * The Name of the Default ItemData
+     */
     public static final ConfigKey<String> NAME =
-            new ConfigKey<>(new String[] {"name"}, new ConfigStringValue("Defaultname"));
+        new ConfigKey<>(new String[]{"name"}, new ConfigStringValue("Defaultname"));
 
-    /** The Type of the Default ItemData */
-    public static final ConfigKey<ItemType> TYPE =
-            new ConfigKey<>(new String[] {"type"}, new ConfigEnumValue<>(ItemType.Basic));
+    /**
+     * The Type of the Default ItemData
+     */
+    public static final ConfigKey<ItemClassification> TYPE =
+        new ConfigKey<>(new String[]{"type"}, new ConfigEnumValue<>(ItemClassification.Basic));
 
-    /** The texturepath of the Default ItemData will be used for world and Inventory */
+    /**
+     * The texturepath of the Default ItemData will be used for world and Inventory
+     */
     public static final ConfigKey<String> TEXTURE =
-            new ConfigKey<>(
-                    new String[] {"texture"},
-                    new ConfigStringValue("animation/missingTexture.png"));
+        new ConfigKey<>(
+            new String[]{"texture"},
+            new ConfigStringValue("animation/missingTexture.png"));
 }
