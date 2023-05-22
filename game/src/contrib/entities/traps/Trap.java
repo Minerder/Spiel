930a9b30a2b77ccb80cf375decce009ec8ab5ca4
Scepzz <bent.schoene@gmail.com>
2023-05-23 01:41:09 +0200

    add trap superclass
    - refactor SkillComponent javadocs

---
+++ game/src/contrib/entities/traps/Trap.java
@@ -0,0 +1,49 @@
+package contrib.entities.traps;
+
+import core.Entity;
+import core.components.DrawComponent;
+import core.components.PositionComponent;
+import core.utils.components.draw.Animation;
+import dslToGame.AnimationBuilder;
+
+public class Trap extends Entity {
+
+    private final Animation activation;
+    private final Animation idle;
+    private final DrawComponent dc;
+
+    /**
+     * Creates a new Trap
+     *
+     * @param idleAnimation The path to the idle animation
+     * @param activationAnimation The path to the activation animation
+     */
+    public Trap(String idleAnimation, String activationAnimation) {
+        super();
+        activation = AnimationBuilder.buildAnimation(activationAnimation);
+        idle = AnimationBuilder.buildAnimation(idleAnimation);
+        dc = new DrawComponent(this, idle);
+    }
+
+    /**
+     * Sets up the position component of the trap
+     */
+    public void setupPositionComponent() {
+        new PositionComponent(this);
+    }
+
+    /**
+     * Sets the animation of the trap to the activation animation
+     */
+    public void setActivationAnimation() {
+        if (dc != null) dc.setCurrentAnimation(activation);
+    }
+
+    /**
+     * Sets the animation of the trap to the idle animation
+     */
+    public void setIdleAnimation() {
+        if (dc != null) dc.setCurrentAnimation(idle);
+    }
+
+}
